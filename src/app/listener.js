499aabe03c7d0feb074e56380587ad55876bd2ed
dimas af <dimasaf.ico@gmail.com>
2021-08-16 18:37:40 +0700

    add cart features

--- src/app/listener.js
+++ src/app/listener.js
@@ -1,17 +1,34 @@
+// berfungsi menyumpan ke localstorage
+
 import store from "./store";
+import { saveCart } from '../api/cart';
 
 let currentAuth;
+let currentCart;
 
 function listener(){
 	let previusAuth = currentAuth;
+	let previousCart = currentCart;
 
 	currentAuth = store.getState().auth;
+	currentCart = store.getState().cart;
+
+	let { token } = currentAuth;
 
 	// cek apakah nilai state `auth` berubah dari nilai sebelumnya
 	if(currentAuth !== previusAuth){
 		// jika berubah simpan ke local storage
 		localStorage.setItem('auth', JSON.stringify(currentAuth))
+
+		saveCart(token, currentCart);
 	}
+
+	if(currentCart !== previousCart){
+		localStorage.setItem('cart', JSON.stringify(currentCart));
+
+		saveCart(token, currentCart);
+	}
+
 }
 
 function listen(){
--- src/app/store.js
+++ src/app/store.js
@@ -3,6 +3,8 @@ import thunk from 'redux-thunk';
 
 import authReducer from '../features/Auth/reducer';
 import productReducer from '../features/Products/reducer'
+import cartReducer from '../features/Cart/reducer';
+
 
 // buat composer enhancer untuk menghubungkan dengan Chrome DevTools Redux
 const composerEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
@@ -10,7 +12,8 @@ const composerEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const rootReducers = combineReducers({
 	auth: authReducer,
-	products: productReducer
+	products: productReducer,
+	cart: cartReducer
 });
 //  buat store, dan gunakan composerEnhancer + middleware thunk
 const store = createStore(rootReducers, composerEnhancer(applyMiddleware(thunk)));
